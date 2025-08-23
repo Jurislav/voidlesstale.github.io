c1ec97bceb6d550343c0817300f2c14fe616b0fc
Juris <srcgames@gmail.com>
2025-08-23 22:36:33 +0300

    Add Tailwind configuration and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  site: 'https://www.voidlesstale.com',
+  site: 'https://jurislav.github.io', // set to your custom domain once CNAME is live
+  output: 'static',
 });
